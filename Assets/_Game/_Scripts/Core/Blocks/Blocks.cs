a6aa55c3f5b469af98a3a542980cdf8d83df45eb
Yackov <banzai.davai@gmail.com>
2026-04-11 01:05:36 +0500

    #3

---
+++ Assets/_Game/_Scripts/Core/Blocks/Blocks.cs
@@ -0,0 +1,29 @@
+﻿using _Game._Scripts.Core.Data.ScriptableObjects;
+using UnityEngine;
+
+namespace _Game._Scripts.Core.Blocks
+{
+    public class Block
+    {
+        private readonly BlockData _blockData;
+
+        private float _currentDestroyTime;
+
+        public Block(BlockData blockData)
+        {
+            _blockData = blockData;
+        }
+
+        public bool Destruction(float speedToDestroy)
+        {
+            if (_currentDestroyTime < _blockData.TimeToDestroy)
+            {
+                _currentDestroyTime += speedToDestroy + Time.deltaTime;
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+}
---
+++ Assets/_Game/_Scripts/Core/Blocks/MultiBlock.cs
@@ -0,0 +1,15 @@
+﻿using _Game._Scripts.Core.Data.ScriptableObjects;
+using UnityEngine;
+
+namespace _Game._Scripts.Core.Blocks
+{
+    public class MultiBlock : Block
+    {
+        private readonly Vector2Int[] _blockPositions;
+
+        public MultiBlock(BlockData blockData, Vector2Int[] blockPositions) : base(blockData)
+        {
+            _blockPositions = blockPositions;
+        }
+    }
+}
